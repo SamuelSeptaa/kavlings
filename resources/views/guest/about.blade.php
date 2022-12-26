e97457962c9d633afe52ae98f27f781238c4a277
Samuel Septa <samuelseptaa@gmail.com>
2022-12-26 18:09:00 +0700

    revisi done

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -21,53 +21,46 @@
         <div class="row">
             <div class="col-lg-7">
                 <div class="featured-text">
-                    <h2 class="pb-3">Why <span class="orange-text">Fruitkha</span></h2>
+                    <h2 class="pb-3">Why <span class="orange-text">Choose Us</span></h2>
                     <div class="row">
                         <div class="col-lg-6 col-md-6 mb-4 mb-md-5">
-                            <div class="list-box d-flex">
+                            <div class="list-box d-flex align-items-center">
                                 <div class="list-icon">
                                     <i class="fas fa-shipping-fast"></i>
                                 </div>
                                 <div class="content">
-                                    <h3>Home Delivery</h3>
-                                    <p>sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa
-                                        quae ab illo.</p>
+                                    <h3>Pemesanan Online</h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 mb-5 mb-md-5">
-                            <div class="list-box d-flex">
+                            <div class="list-box d-flex align-items-center">
                                 <div class="list-icon">
                                     <i class="fas fa-money-bill-alt"></i>
                                 </div>
                                 <div class="content">
-                                    <h3>Best Price</h3>
-                                    <p>sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa
-                                        quae ab illo.</p>
+                                    <h3>Harga Terbaik</h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 mb-5 mb-md-5">
-                            <div class="list-box d-flex">
+                            <div class="list-box d-flex align-items-center">
                                 <div class="list-icon">
                                     <i class="fas fa-briefcase"></i>
                                 </div>
                                 <div class="content">
-                                    <h3>Custom Box</h3>
-                                    <p>sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa
-                                        quae ab illo.</p>
+                                    <h3>Tersedia Add Ons</h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6">
-                            <div class="list-box d-flex">
+                            <div class="list-box d-flex align-items-center">
                                 <div class="list-icon">
                                     <i class="fas fa-sync-alt"></i>
                                 </div>
                                 <div class="content">
-                                    <h3>Quick Refund</h3>
-                                    <p>sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa
-                                        quae ab illo.</p>
+                                    <h3>Proses Cepat</h3>
+
                                 </div>
                             </div>
                         </div>
